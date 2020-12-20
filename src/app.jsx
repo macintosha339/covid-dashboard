f976a71d07e35d08473808451ca3ef0348c40932
AntonMihailov <you@example.com>
2020-12-21 01:57:05 +0300

    init: start project

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1 className="react">
+        Hello
+      </h1>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import './styles/body.scss';
+import './styles/reset.css';
+
+import './app';
